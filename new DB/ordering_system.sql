cf200db424a23909423dd64df5b5fae6d88e7570
richard <35753309+hayes-seyah09354085977@users.noreply.github.com>
2019-09-08 21:37:55 +0800

    new db

--- new DB/ordering_system.sql
+++ new DB/ordering_system.sql
@@ -46,13 +46,14 @@ CREATE TABLE `tblcategory` (
   `Categories` varchar(90) NOT NULL,
   `StoreID` int(11) NOT NULL,
   PRIMARY KEY (`CategoryID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tblcategory` */
 
 insert  into `tblcategory`(`CategoryID`,`Categories`,`StoreID`) values 
-(3,'Gamot Sa Ubo',2101),
-(4,'ilong',2101);
+(5,'Watches',2101),
+(6,'T-shirts',2101),
+(7,'Necklace',2101);
 
 /*Table structure for table `tblcustomer` */
 
@@ -61,6 +62,7 @@ DROP TABLE IF EXISTS `tblcustomer`;
 CREATE TABLE `tblcustomer` (
   `CustomerID` int(11) NOT NULL AUTO_INCREMENT,
   `CustomerName` varchar(90) NOT NULL,
+  `Email` varchar(150) NOT NULL,
   `CustomerAddress` varchar(90) NOT NULL,
   `CustomerContact` varchar(90) NOT NULL,
   `Sex` varchar(30) NOT NULL,
@@ -68,23 +70,12 @@ CREATE TABLE `tblcustomer` (
   `Customer_Password` varchar(90) NOT NULL,
   `Customer_Photo` varchar(90) NOT NULL,
   PRIMARY KEY (`CustomerID`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=570 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tblcustomer` */
 
-insert  into `tblcustomer`(`CustomerID`,`CustomerName`,`CustomerAddress`,`CustomerContact`,`Sex`,`Customer_Username`,`Customer_Password`,`Customer_Photo`) values 
-(1,'qweqweqwe','','','Female','','da39a3ee5e6b4b0d3255bfef95601890afd80709',''),
-(2,'asd','','','Female','','da39a3ee5e6b4b0d3255bfef95601890afd80709',''),
-(3,'asde','asd','123213','Female','a','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8',''),
-(4,'21','asdsad','213','Female','a','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8',''),
-(5,'','','','','','da39a3ee5e6b4b0d3255bfef95601890afd80709',''),
-(6,'asd','asd','12312312','Female','a','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8',''),
-(7,'sasad','asdas','213123','Female','a','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8',''),
-(8,'asdsad','asdasd','123123','Male','r','4dc7c9ec434ed06502767136789763ec11d2c4b7',''),
-(9,'a','a','a','Female','a','86f7e437faa5a7fce15d1ddcb9eaeaea377667b8',''),
-(10,'saadasd','asdasd','213','Male','c','84a516841ba77a5b4648de2cd0dfcb30ea46dbb4','photos/2019-01-13 (2).png'),
-(11,'Bebe\'s Store','Kabankalan City, Negros Occidental, Philippines','9012312312','Female','bebe','b6204a75b33ab44405d3c00d38a1fd3f67ac2706',''),
-(12,'Richard De Vera','test','test123123','Male','test','a94a8fe5ccb19ba61c4c0873d391e987982fbbd3','photos/Capture.PNG');
+insert  into `tblcustomer`(`CustomerID`,`CustomerName`,`Email`,`CustomerAddress`,`CustomerContact`,`Sex`,`Customer_Username`,`Customer_Password`,`Customer_Photo`) values 
+(569,'charlie libaton','test@gmail.com','Test Address','09451245686','Male','qwe','056eafe7cf52220de2df36845b8ed170c67e23e3','');
 
 /*Table structure for table `tblinventory` */
 
@@ -98,7 +89,7 @@ CREATE TABLE `tblinventory` (
   `Remaining` int(11) NOT NULL,
   PRIMARY KEY (`TransID`),
   UNIQUE KEY `ProductID` (`ProductID`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tblinventory` */
 
@@ -112,8 +103,8 @@ insert  into `tblinventory`(`TransID`,`ProductID`,`Stocks`,`Sold`,`Remaining`) v
 (10,14,0,0,0),
 (11,1,90,0,90),
 (12,2,78,10,68),
-(13,3,11107,7,11100),
-(14,4,22,0,22);
+(15,5,15,0,15),
+(16,6,15,0,15);
 
 /*Table structure for table `tblloan` */
 
@@ -150,13 +141,13 @@ CREATE TABLE `tblproduct` (
   `Image2` text NOT NULL,
   `Image3` text NOT NULL,
   PRIMARY KEY (`ProductID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tblproduct` */
 
 insert  into `tblproduct`(`ProductID`,`ProductName`,`Description`,`Price`,`DateExpire`,`CategoryID`,`StoreID`,`Image1`,`Image2`,`Image3`) values 
-(3,'test','test',2321312312,'2019-09-12',3,2101,'photos/FYGA6QAIRO1DN0F.LARGE.jpg','photos/','photos/'),
-(4,'qqqq','ww',111,'2019-09-02',4,2101,'photos/','photos/287474.jpg','photos/');
+(5,'G-shock','G-SHOCK shock resistant military and tactical watches with outstanding water resistant fea',3200,'2019-09-28',5,2101,'photos/Gents-Watches.jpg','photos/Silks-sents.jpg','photos/watches.jpg'),
+(6,'T-shirt Model 1','Tshirt deisgn',3200,'2019-09-21',6,2101,'photos/penshoppe.jpg','photos/','photos/');
 
 /*Table structure for table `tblstockin` */
 
@@ -168,14 +159,14 @@ CREATE TABLE `tblstockin` (
   `Quantity` int(11) NOT NULL,
   `DateReceive` date NOT NULL,
   PRIMARY KEY (`StockinID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tblstockin` */
 
 insert  into `tblstockin`(`StockinID`,`ProductID`,`Quantity`,`DateReceive`) values 
 (2,2,78,'2019-02-03'),
-(3,3,11107,'2019-09-01'),
-(4,4,22,'2019-09-01');
+(5,5,15,'2019-09-02'),
+(6,6,15,'2019-09-02');
 
 /*Table structure for table `tblstockout` */
 
@@ -192,17 +183,14 @@ CREATE TABLE `tblstockout` (
   `OrderNo` varchar(90) NOT NULL,
   `HView` tinyint(1) NOT NULL,
   PRIMARY KEY (`StockoutID`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 /*Data for the table `tblstockout` */
 
 insert  into `tblstockout`(`StockoutID`,`CustomerID`,`ProductID`,`Quantity`,`DateSold`,`Status`,`Remarks`,`OrderNo`,`HView`) values 
 (3,12,2,5,'2019-08-28','Cancelled','','6912537',0),
 (4,12,2,3,'2019-08-28','Confirmed','','6912538',0),
-(5,12,2,2,'2019-08-28','Confirmed','','6912539',0),
-(6,12,3,7,'2019-09-01','Cancelled','','6912540',0),
-(7,12,3,2,'2019-09-01','Pending','','6912541',1),
-(8,12,4,3,'2019-09-01','Pending','','6912542',1);
+(5,12,2,2,'2019-08-28','Confirmed','','6912539',0);
 
 /*Table structure for table `tblstore` */
 
@@ -222,9 +210,7 @@ CREATE TABLE `tblstore` (
 /*Data for the table `tblstore` */
 
 insert  into `tblstore`(`StoreID`,`StoreName`,`StoreAddress`,`ContactNo`,`st_Image1`,`st_Image2`,`st_Image3`) values 
-(8,'Jano\'s store','Kabankalan City, Negros Occidental, Philippines','90123123','','',''),
-(9,'Bebe\'s Store','Kabankalan City, Negros Occidental, Philippines','09876890123','','',''),
-(2101,'Admin\'s Store','','','','','');
+(2101,'Watch Ur Toyo','','','','','');
 
 /*Table structure for table `tblsummary` */
 
@@ -282,6 +268,41 @@ CREATE TABLE `tblusers` (
 insert  into `tblusers`(`UserID`,`FullName`,`Username`,`Password`,`Role`,`PicLoc`) values 
 (2101,'ako to','admin','d033e22ae348aeb5660fc2140aec35850c4da997','Administrator','photos/2019-01-22.png'),
 (29838,'','test','a94a8fe5ccb19ba61c4c0873d391e987982fbbd3','Staff','photos/FYGA6QAIRO1DN0F.LARGE.jpg');
+
+/*Table structure for table `tblvarcat` */
+
+DROP TABLE IF EXISTS `tblvarcat`;
+
+CREATE TABLE `tblvarcat` (
+  `varcatid` int(11) NOT NULL AUTO_INCREMENT,
+  `variationcat` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`varcatid`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tblvarcat` */
+
+insert  into `tblvarcat`(`varcatid`,`variationcat`) values 
+(1,'Color'),
+(2,'Size'),
+(8,'Size 2');
+
+/*Table structure for table `tblvariation` */
+
+DROP TABLE IF EXISTS `tblvariation`;
+
+CREATE TABLE `tblvariation` (
+  `varid` int(11) NOT NULL AUTO_INCREMENT,
+  `varcatid` varchar(50) DEFAULT NULL,
+  `variation` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`varid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tblvariation` */
+
+insert  into `tblvariation`(`varid`,`varcatid`,`variation`) values 
+(1,'1','Blue,Black,Green,Red'),
+(2,'2','Small,Medium,Large,Extra Large'),
+(4,'8','1,2,3,4,5,6,7\r\n');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
