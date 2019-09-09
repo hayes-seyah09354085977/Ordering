b88261ab59a682ae72a6f356aab51bafc5311cf6
Chlibaton <charliemagnefrancis.libaton@gmail.com>
2019-09-09 18:04:37 +0800

    te

--- product-view.php
+++ product-view.php
@@ -1,139 +1,139 @@
 
 <style type="text/css">
-#myCarousel {
-  margin-top: 5px;
-}
-.stretch img{ 
- width: 100%;
- height: 50%;
-}
-section#inner-headline {
-    background: #37393c !important;
-}
-p.currency {
-    font-size: 20px !important;
-    font-weight: bold;
-}
-.magnify{
-  border-radius: 50%;
-  border: 2px solid black;
-  position: absolute;
-  z-index: 20;
-  background-repeat: no-repeat;
-  background-color: white;
-  box-shadow: inset 0 0 20px rgba(0,0,0,.5);
-  display: none;
-  cursor: none;
-}
-.product-image {
-  padding: 10px 10px 0 10px;
-}
-
-.product-image img {
+  #myCarousel {
+    margin-top: 5px;
+  }
+  .stretch img{ 
   width: 100%;
+  height: 50%;
+  }
+  section#inner-headline {
+      background: #37393c !important;
+  }
+  p.currency {
+      font-size: 20px !important;
+      font-weight: bold;
+  }
+  .magnify{
+    border-radius: 50%;
+    border: 2px solid black;
+    position: absolute;
+    z-index: 20;
+    background-repeat: no-repeat;
+    background-color: white;
+    box-shadow: inset 0 0 20px rgba(0,0,0,.5);
+    display: none;
+    cursor: none;
+  }
+  .product-image {
+    padding: 10px 10px 0 10px;
+  }
+
+  .product-image img {
+    width: 100%;
+      height: 100%;
+      object-fit: contain;
+      height: 295px;
+  }
+  .product-checkout{left:10px; font-size: 12px; text-transform: uppercase; margin-top: 10px!important;}
+  .product-checkout-actions{ right:0; margin-top: 10px!important;}
+  .product-checkout-total, .product-checkout-total-amount{font-size: 20px; color: #C17A41;}
+  .product-checkout-total * {display:inline-block;}
+  .product-checkout-actions{}
+
+  /* 6. Components - buttons, menus, images, etc. */
+  .product-quantity-label{text-transform:uppercase;}
+  .product-quantity *{display:inline-block;}
+
+  #product-quantity-input{background-color: #eee;border: none; width:2.5em; text-align: center;}
+  .product-quantity-subtract, .product-quantity-add{cursor: pointer; margin-left: 20px; padding-left:5px; padding-right: 5px;}
+  .product-quantity-subtract{margin-right:20px;}
+
+  .product-title {
+    font-size: 30px;
+    font-weight: 600;
+  }
+  .product-price {
+    color: #f5782d;
+    font-size: 25px;
+    font-weight: 600;
+    letter-spacing: 1px;
+  }
+  .product-description {
+      margin: 14px 14px 14px 0px;
+      border-style: solid;
+      border-left: 0px;
+      border-right: 0px;
+      border-top: 0px;
+      border-color:#f5782d;
+  }
+  .product-description,.product-quantity,.product-price {
+    padding: 15px 15px 15px 0px;
+  }
+  #slider {
+      position: relative;
+      overflow: hidden;
+      margin: 20px auto 0 auto;
+      border-radius: 4px;
+  }
+  #slider ul {
+      position: relative;
+      margin: 0;
+      padding: 0;
+      height: 150px;
+      list-style: none;
+      border-top: solid;
+      border-color: #f5782d;  
+  }
+  #slider ul li {
+      position: relative;
+      display: block;
+      float: left;
+      margin: 10px;
+      padding: 0;
+      width: 80px;
+      height: 80px;
+      background: #ccc;
+      text-align: center;
+      border: solid;
+      border-color: #37393c;
+  }
+  .imgmini{
+    width:100%;
+    max-width:100px;
     height: 100%;
-    object-fit: contain;
-    height: 295px;
-}
-.product-checkout{left:10px; font-size: 12px; text-transform: uppercase; margin-top: 10px!important;}
-.product-checkout-actions{ right:0; margin-top: 10px!important;}
-.product-checkout-total, .product-checkout-total-amount{font-size: 20px; color: #C17A41;}
-.product-checkout-total * {display:inline-block;}
-.product-checkout-actions{}
-
-/* 6. Components - buttons, menus, images, etc. */
-.product-quantity-label{text-transform:uppercase;}
-.product-quantity *{display:inline-block;}
-
-#product-quantity-input{background-color: #eee;border: none; width:2.5em; text-align: center;}
-.product-quantity-subtract, .product-quantity-add{cursor: pointer; margin-left: 20px; padding-left:5px; padding-right: 5px;}
-.product-quantity-subtract{margin-right:20px;}
-
-.product-title {
-  font-size: 30px;
-  font-weight: 600;
-}
-.product-price {
-  color: #f5782d;
-  font-size: 25px;
-  font-weight: 600;
-  letter-spacing: 1px;
-}
-.product-description {
-    margin: 14px 14px 14px 0px;
-    border-style: solid;
-    border-left: 0px;
-    border-right: 0px;
-    border-top: 0px;
-    border-color:#f5782d;
-}
-.product-description,.product-quantity,.product-price {
-  padding: 15px 15px 15px 0px;
-}
-#slider {
-    position: relative;
-    overflow: hidden;
-    margin: 20px auto 0 auto;
-    border-radius: 4px;
-}
-#slider ul {
-    position: relative;
-    margin: 0;
-    padding: 0;
-    height: 150px;
-    list-style: none;
-    border-top: solid;
-    border-color: #f5782d;  
-}
-#slider ul li {
-    position: relative;
-    display: block;
-    float: left;
-    margin: 10px;
-    padding: 0;
-    width: 80px;
-    height: 80px;
-    background: #ccc;
-    text-align: center;
-    border: solid;
-    border-color: #37393c;
-}
-.imgmini{
-  width:100%;
-  max-width:100px;
-  height: 100%;
-}
-input.remqty {
-    border: none;
-}
-.variation {
-    padding-top: 15px;
-}
-.col-sm-2.title {
-    margin: 10px 0px 0px 0px;
-}
-.options {
-    border: 1px solid;
-    border-color:#b1b1b1;
-    width: 98px;
-    max-width: 100%;
-    font-size: 12px;
-    text-align: center;
-    cursor:pointer;
-    margin: 5px;
-    padding: 5px;
-}
-.options:hover {
-    background: #f5782d;
-    color: white;
-}
-.activeOpts{
-  background:#f5782d;
-  color:#fff;
-}
-.row {
-    margin-bottom: 15px !important;
-}
+  }
+  input.remqty {
+      border: none;
+  }
+  .variation {
+      padding-top: 15px;
+  }
+  .col-sm-2.title {
+      margin: 10px 0px 0px 0px;
+  }
+  .options {
+      border: 1px solid;
+      border-color:#b1b1b1;
+      width: 98px;
+      max-width: 100%;
+      font-size: 12px;
+      text-align: center;
+      cursor:pointer;
+      margin: 5px;
+      padding: 5px;
+  }
+  .options:hover {
+      background: #f5782d;
+      color: white;
+  }
+  .activeOpts{
+    background:#f5782d;
+    color:#fff;
+  }
+  .row {
+      margin-bottom: 15px !important;
+  }
 
 </style>
 
@@ -326,10 +326,4 @@ $.ajax({
       }
   });
 
-
-
-
-      
 </script>
-
-
